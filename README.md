3729b887ffdbf2573b97ee87fd662e8901716ef0
srish-kumar <srish_seed@yahoo.com>
2024-09-14 08:06:37 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# kubernetes
+1. Create an EC2 instance - Ubuntu 22
+2. Setup security group having inbound rules for 
+   Jenkins - Custom TCP/TCP/8080
+   Sonarqube - Custom TCP/TCP/9000
+   HTTP - HTTP/TCP/80
+   SSH - SSH/TCP/22
+2. Install all the required tools:
+    Jenkins
+    AWS CLI
+    Docker
+    Terraform
+    Sonarqube
+    Trivy
+3. Connect to the EC2 usnig AWS Systems Manager Session Manager and validate tool installation.
+3. Access the Jenkins server in a browser - <public ip of EC2 instance>:<Jenkins port>
+4.  Get Jenkins password by running command in session manager:
+    $ sudo systemctl status jenkins.service (sudo may be required to view journal files)
+5. Install suggested plugins 
+6. On the Jenkins Dashboard, select Manage Jenkins to install plugins.
+7. Install these plugins for deploying infrastructure on AWS:
+    AWS Credentials
+    Pipeline: AWS Steps
+8. After installation we need to save AWS credentials, go to manage jenkins -> security - > Credentials
+   global -> add credentials 
+   Under kind - Select AWS Credentials (available if AWS credentials plugin is installed)
+9. Create an IAM user with admin access to get the Access and Secret keys. Provide info in Jenkins.
+10. Now install Terraform plugin
+11. Goto Manage Jenkins -> Tools -> Terraform installations
+    As we already have terraform installed on the Jenkins server, we will use the same for installation
+    From Session run command - $whereis terraform (this will provide the directory path where terrafrom is)
+    /usr/bin/terraform
+    Uncheck the Install Automatically checkbox and paste the file path.
+    Click apply and save.
+12. Install Jenkins plugin -  Pipeline: Stage View
+13. Now we will create a pipeline to run terraform to create EKS infra.
+
+
+
+
+
+
---
+++ install-tools.sh
@@ -0,0 +1,111 @@
+Script to install tools:
+
+#!/bin/bash
+# For Ubuntu 22.04
+
+# Installing Java for Jenkins
+sudo apt update -y
+sudo apt install openjdk-17-jre -y
+sudo apt install openjdk-17-jdk -y
+java --version
+# Installing Jenkins
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+# Installing Docker
+#!/bin/bash
+sudo apt update
+sudo apt install docker.io -y
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+sudo chmod 777 /var/run/docker.sock
+
+# Installing Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+sudo apt update
+sudo apt install terraform
+
+# Installing AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+
+
+# To Run SonarQube as container run command
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+# Installing Trivy
+#!/bin/bash
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+
+================================
+
+# # Install kubectl:
+# #! /bin/bash
+# sudo apt update
+# sudo apt install curl
+# curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# kubectl version --client
+
+# ========================
+
+# # Install-Jfrog-Artifactory
+
+# #! /bin/bash
+
+# cd /opt
+# wget https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/7.9.2/jfrog-artifactory-oss-7.9.2-linux.tar.gz
+# tar -xvf jfrog-artifactory-oss-7.9.2-linux.tar.gz
+# cd artifactory-oss-7.9.2/
+# cd app/bin/
+# ./artifactory.sh start
+
+# =======================
+
+# # Install minikube
+# #! /bin/bash
+
+# sudo apt update
+# sudo apt upgrade
+# wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+# chmod +x minikube-linux-amd64
+# sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+# ==============
+
+# # Note:
+# # Installation Only Work for Ubuntu 18.04LTS, 20.04LTS and 22.04LTS
+
+# # Steps to use the Scripts For Linux OS only
+# # Installation Only Work for Ubuntu 18.04LTS, 20.04LTS and 22.04LTS
+
+# # Steps to use the Scripts For Linux OS only
+# # Clone the repo by command, git clone where the scripts are placed in a folder 
+# # called Sripts-Installation, ex Install-Jenkins.sh, Install-Docker.sh etc.
+
+# cd Scripts-Installation
+# sudo chmod +x <Script_name> e.g,sudo chmod +x Install-Docker.sh
+# ./<Script_name> e.g ./Install-Docker.sh
+
+
+
