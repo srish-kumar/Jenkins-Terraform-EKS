73dac90170d5c9111dc4f06afbfbc769253e0e8c
srish-kumar <srish_seed@yahoo.com>
2024-09-16 19:54:13 -0400

    change in tools file

--- install-tools.sh
+++ install-tools.sh
@@ -1,4 +1,4 @@
-Script to install tools:
+#Script to install tools on the Jenkins server:
 
 #!/bin/bash
 # For Ubuntu 22.04
@@ -46,39 +46,41 @@ sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
-
-# To Run SonarQube as container run command
+# To Run SonarQube as container on port 9000
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
-# Installing Trivy
+
+# Installing Trivy for image scaning
 #!/bin/bash
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
-sudo apt-get install trivy
+sudo apt-get install trivy -y
 
-================================
 
+============================
 # # Install kubectl:
-# #! /bin/bash
+# # To handle eks cluster and resources with kubectl
+# #!/bin/bash
 # sudo apt update
-# sudo apt install curl
-# curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
-# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# sudo apt install curl -y
+# curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+# sudo chmod +x kubectl
+# sudo mv kubectl /usr/local/bin/
 # kubectl version --client
 
-# ========================
 
 # # Intalling Helm
-# #! /bin/bash
+# # To be used in the monitoring part
+# #!/bin/bash
 # sudo snap install helm --classic
 
-==========================
+
 
 # # Install-Jfrog-Artifactory
 
-# #! /bin/bash
+# #!/bin/bash
 
 # cd /opt
 # wget https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/7.9.2/jfrog-artifactory-oss-7.9.2-linux.tar.gz
@@ -90,7 +92,7 @@ sudo apt-get install trivy
 # =======================
 
 # # Install minikube
-# #! /bin/bash
+# #!/bin/bash
 
 # sudo apt update
 # sudo apt upgrade
